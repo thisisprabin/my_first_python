2ba2a9922eb81bf06920c331e46ba4298e2dc21a
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2016-10-30 02:09:47 +0530

    Update Class.py

--- Class.py
+++ Class.py
@@ -3,31 +3,31 @@ class MyFirst:
     def __init__(self, name, age):
         self.name = name;
         self.age = age;
-        MyFirst.emp += 1
+        MyFirst.emp += 1;
     
     def displayName(self):
-        print("Person name ", self.name," ", MyFirst.emp)
+        print("Person name ", self.name," ", MyFirst.emp);
         return;
     
     def displayAge(self):
-        print("Person age ", self.age)
+        print("Person age ", self.age);
         return;
         
-name = "Prabin"
-age = 21
+name = "Prabin";
+age = 21;
 
-obj = MyFirst(name, age)
-obj.displayName()
-obj.displayAge() 
+obj = MyFirst(name, age);
+obj.displayName();
+obj.displayAge(); 
 
-print(getattr(obj, 'age'))
-print(hasattr(obj, 'age'))
-print(setattr(obj, 'age', 34))
-obj.displayAge()
+print(getattr(obj, 'age'));
+print(hasattr(obj, 'age'));
+print(setattr(obj, 'age', 34));
+obj.displayAge();
 
-print("Docmentation ", MyFirst.__doc__)
+print("Docmentation ", MyFirst.__doc__);
 print("Dictionary ", MyFirst.__dict__);
 print("Name ", MyFirst.__name__);
 print("Module ", MyFirst.__module__);
-print("Base ", MyFirst.__base__)
-
+print("Base ", MyFirst.__base__);
+print(id(obj));
