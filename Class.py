eb3fe992e766aab80d3ebcc5894a517b2a87602e
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2016-10-30 01:53:46 +0530

    Update Class.py

--- Class.py
+++ Class.py
@@ -1,18 +1,27 @@
 class MyFirst:
+    emp = 0
     def __init__(self, name, age):
         self.name = name;
         self.age = age;
+        MyFirst.emp += 1
+    
     def displayName(self):
-        print("Person name ", self.name)
+        print("Person name ", self.name," ", MyFirst.emp)
+        return;
+    
     def displayAge(self):
         print("Person age ", self.age)
+        return;
         
-
-print("Enter the name")
-name = input()
-print("Enter the age")
-age = int(input())
+name = "Prabin"
+age = 21
 
 obj = MyFirst(name, age)
 obj.displayName()
-obj.displayAge()            
+obj.displayAge() 
+
+print(getattr(obj, 'age'))
+print(hasattr(obj, 'age'))
+print(setattr(obj, 'age', 34))
+obj.displayAge()
+print(delattr(obj, 'age'))
