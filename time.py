7b7b0c2fc4eddda5c4d084eb4629e6a60ec1bffa
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2017-02-08 15:17:49 +0530

    Delete time.py

--- time.py
+++
@@ -1,6 +0,0 @@
-import time, calendar
-a = time.localtime(time.time())
-print(time.localtime(time.time()))
-print(a.tm_mon," ",a.tm_hour)
-print(time.asctime(time.localtime(time.time())))
-print(calendar.month(1991, 12))
