3d6f90521348bfe65d73a14adaa9c10cb001affc
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2017-02-11 01:25:29 +0530

    Add files via upload

---
+++ Python-3.14-Dictionary/Test.py
@@ -0,0 +1,13 @@
+dictionary = {'name':'charlie', 'id':100, 'dept':'it'}
+print(dictionary)
+print(dictionary.keys())
+print(dictionary.values())
+
+data1={'Id':100, 'Name':'Suresh', 'Profession':'Developer'}
+data2={'Id':101, 'Name':'Ramesh', 'Profession':'Trainer'}
+data1['Profession']='Manager'
+data2['Salary']=20000
+data1['Salary']=15000
+print(data1)
+print(data2)
+
